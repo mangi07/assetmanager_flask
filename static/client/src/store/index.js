b936abecf6bdab043407033eba92aaf31e4cd4dd
mangi07 <>
2019-09-24 17:02:04 +1000

    small fix for vuex store, TODO: test it out

--- static/client/src/store/index.js
+++ static/client/src/store/index.js
@@ -1,38 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { userModule } from './modules/user.module'
 
 Vue.use(Vuex)
 
-function resetUser (user, new_user=null) {
-  if (new_user == null) {
-    user.username = null
-    user.role = null
-    user.loggedIn = false
-    user.error = null
-    return;
-  }
-  user.username = new_user.username
-  user.role = new_user.role
-  user.loggedIn = new_user.loggedIn
-  user.error = new_user.error
-}
-
 export default new Vuex.Store({  
-  state: {
-    user: {
-      username: null,
-      role: null,
-      loggedIn: false,
-      error: null,
-    },
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    userModule,
   },
-  mutations: {
-    setUser (state, user) {
-      resetUser(state.user, user)
-    },
-    unsetUser (state) {
-      resetUser(state.user)
-    }
-  }
 })
 
