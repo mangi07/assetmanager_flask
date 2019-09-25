01d623b277d70c6c4e1314d63afb7c9cdb296af0
mangi07 <>
2019-09-25 15:46:04 +1000

    adjusting python environment setup files

---
+++ conda.ps1
@@ -0,0 +1,10 @@
+# C:\Users\<user.name>\AppData\Local\Continuum\anaconda3\shell\condabin\conda-hook.ps1
+# conda activate 'C:\Users\<user.name>\AppData\Local\Continuum\anaconda3'
+
+# conda create --name flaskenv python=3.7 pip=19.2.3
+# OR
+# conda env create -f condaenv.yml
+
+conda activate flaskenv
+
+# conda deactivate
