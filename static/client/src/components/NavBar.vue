4a274155bf49505650b1f27eed78e0eae4fbcbc2
mangi07 <>
2019-10-04 17:24:51 +1000

    learning vuetify

--- static/client/src/components/NavBar.vue
+++ static/client/src/components/NavBar.vue
@@ -1,7 +1,41 @@
 <template>
   <div>
-    <v-toolbar>
-      <v-toolbar-title>Asset Manager <span v-if="user.loggedIn">User: {{ user.username }}, Role: {{ user.role }}</span></v-toolbar-title>
+    <v-app-bar>
+      <v-app-bar-nav-icon>
+        <v-navigation-drawer
+          v-model="drawerRight"
+          app
+          clipped
+          right
+        >
+          <v-list dense>
+            <v-list-item @click.stop="right = !right">
+              <v-list-item-action>
+                <v-icon>mdi-exit-to-app</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-app-bar-nav-icon>
+
+
+      <v-toolbar-title>
+        Asset Manager
+      </v-toolbar-title>
+
+      <div v-if="user.loggedIn">
+        <v-chip>
+          <v-icon left color="red">mdi-account</v-icon>
+          User: {{ user.username }}
+        </v-chip>
+        <v-chip>
+          <v-icon left color="red">mdi-information</v-icon>
+          Role: {{ user.role }}
+        </v-chip>
+      </div>
 
       <div class="flex-grow-1"></div>
 
@@ -12,7 +46,26 @@
         </v-btn>
       </v-toolbar-items>
 
-    </v-toolbar>
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            @click='logOut'
+          >
+            <v-list-item-title>LOGOUT</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+    </v-app-bar>
   </div>
 </template>
 
