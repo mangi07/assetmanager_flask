2d55f592dbd0dd4ab634e52c5f3da572621903d2
mangi07 <>
2019-11-25 13:56:41 +1000

    still refactoring db utils for m2m inserts

--- queries/query_utils.py
+++ queries/query_utils.py
@@ -42,78 +42,102 @@ def list_accounts():
     return _list_categories(db_path, 'account', 'number')
 
 
-# TODO: test
-def parse_db_args_tuple(args):
+################################################
+# m2m inserts
+################################################
+# # TODO: test
+# def _parse_db_args_tuple(args):
+#     """
+#     args: tuple of format ('table', [('col1':val1,...)])
+#         where vals can be str, int, or float
+
+#     return: tuple (table name, column names, column values, question marks)
+#         eg: ('table', 'col1, col2', ('val1', 2), '?, ?')
+#     """
+#     table_name = args[0]
+#     cols = list(zip(args[1]))
+#     col_names = ', '.join(cols[0])
+#     col_vals = cols[1]
+#     col_prep = ', '.join('?'*len(col_names))
+
+#     return (table_name, col_names, col_vals, col_prep)
+
+
+# TODO: finish and test
+def _parse_db_args_dict(args):
     """
-    args: tuple of format ('table', [('col1':val1,...)])
+    args: dict of format ('table', [{'col1':val1,...}])
         where vals can be str, int, or float
 
     return: tuple (table name, column names, column values, question marks)
         eg: ('table', 'col1, col2', ('val1', 2), '?, ?')
-    """
-    table_name = args[0]
-    cols = list(zip(args[1]))
-    col_names = ', '.join(cols[0])
-    col_vals = cols[1]
-    col_prep = ', '.join('?'*len(col_names))
-
-    return (table_name, col_names, col_vals, col_prep)
-
-
-# TODO: finish and test
-def parse_db_args_dict(args):
-    """
-    args: dict of format ('table', {'col1':val1,...})
-        where vals can be str, int, or float
-
-    return: tuple (table name, column names, column values, question marks)
-        eg: ('table', 'col1, col2', ('val1', 2), '?, ?')
-    """
-    pass
-
-
-# TODO: finish and test
-def parse_db_args(args, args_format):
-    """
-    args: tuple or dict with info to be used to create new row in table
-    args_format: 0 (for tuple) or 1 (for dict), governing which method to use to parse incoming args
     
-    example tuple args: ('table', [('col1':val1,...)])
-    example dict args:  ('table', {'col1':val1,...})
+
+    Throws error if each dict does not contain the same number of pairs with the same set of keys
+    
+    Note: The order of keys supplied in each dict does not matter, as long as each dict
+    contains the same set of keys.
     """
-    pass
+    table = args[0]
+    rows = args[1]
+    keys = sorted(rows[0].keys())
+    vals = []
+    for d in rows:
+        v = [d[k] for k in keys]
+        vals.append(tuple(v))
+    cols = ', '.join(keys)
+    col_prep = ', '.join('?'*len(keys))
+    return (table, cols, vals, col_prep)
+
+
+# # TODO: finish and test
+# def parse_db_args(args, args_format):
+#     """
+#     args: tuple or dict with info to be used to create new row in table
+#     args_format: 0 (for tuple) or 1 (for dict), governing which method to use to parse incoming args
+    
+#     example tuple args: ('table', [('col1':val1,...)])
+#     example dict args:  ('table', {'col1':val1,...})
+#     """
+#     pass
 
 
 # TODO: finish and test...finish reworking this function to allow associating, for example, one invoice with multiple assets
-def db_add_m2m_assoc(assoc, m2m, table, a_ids):
+def db_add_m2m_assoc(assoc, m2ms, existing_col):
     """
-    assoc: tuple representing row to insert and associate with asset,
+    assoc: tuple representing new row to insert,
         eg: ('tablename', [('col1', 3), ('col2', 'abc')])
-        Warning: Do not supply id.  This function will find the correct id for row to associate with asset.
+        Warning: Do not supply id.  This function will find the correct id for the new row.
 
-    m2m: tuple representing table that links asset row and assoc
-        eg: ('m2mtablename', [('col1', 3), ('col2', 'abc')])
-        Warning: Only supply details of the relation.  This function will use the correct relation ids.
+    m2ms: tuple representing rows to insert into m2m table,
+        eg: ('m2mtablename', [{'rel_id':3, 'details_of_relation':'details here'}] )
+        Warning: Caller only provides per association the existing id and details of association.
 
-    table: the name of the table with existing entity
+    existing_col: the column name in m2m table representing the existing entity
+
     return: True if successful, otherwise false
     """
     (assoc_table_name, col_names, col_vals, col_prep) = parse_db_args(assoc)
-    (m2m_table_name, m2m_col_names, m2m_col_vals, m2m_col_prep) = parse_db_args(m2m)
+    (m2m_table_name, m2m_col_names, m2m_col_vals, m2m_col_prep) = _parse_db_args_dict(m2ms)
 
     max_assoc_id = get_max_id(assoc_table_name)
     next_assoc_id = max_assoc_id + 1
+
+
 
     try:
         conn = sqlite3.connect(db_path)
         conn.set_trace_callback(print) # TODO: remove this log when done debugging/testing
         c = conn.cursor()
-        c.execute(f"insert into {assoc_table_name} (id, {col_names}) 
-            values (?, {col_prep});", 
+        # new item to be associated
+        c.execute(f"""insert into {assoc_table_name} (id, {col_names}) 
+            values (?, {col_prep});""", 
             (next_assoc_id, ) + col_vals )
-        c.execute(f"""insert into {m2m_table_name} (asset, {assoc_table_name}, {m2m_colnames}) 
-            values (?, ?, {m2m_col_prep});""", 
-            (a_id, next_assoc_id) + m2m_col_vals )
+        
+        # existing item(s) with which new item will be associated
+        row_vals = [(next_assoc_id,) + m2m_col_vals]
+        c.executemany(f"""insert into {m2m_table_name} ({table}, {m2m_col_names}) 
+            values ({m2m_col_prep});""", m2m_col_vals )
         # conn.commit() # TODO: uncomment once sql is tested and working properly
         conn.close()
         return True
--- tests/test_queries_query_utils.py
+++ tests/test_queries_query_utils.py
@@ -67,3 +67,94 @@ class TestQueryUtils:
         """
         cats = query_utils._list_categories('department', 'name')
         assert cats == []
+
+################################################
+# m2m inserts
+################################################
+    def test__parse_db_args_dict_1(self, db_conn):
+        """
+        Parses ('asset_invoice', [{'asset':1, 'cost':100}])
+        Returns: ('asset_invoice', ['asset', 'cost'], [1, 100], '?, ?')
+        """
+        input = ('asset_invoice', [{'asset':1, 'cost':100}])
+        (table, cols, vals, prep) = query_utils._parse_db_args_dict(input)
+        assert table == 'asset_invoice'
+        assert cols == 'asset, cost'
+        assert vals == [(1, 100)]
+        assert prep == '?, ?'
+    
+    def test__parse_db_args_dict_2(self, db_conn):
+        """
+        Parses ('asset_invoice', [{'asset':1, 'cost':100}, {'asset':40, 'cost':150.29}])
+        Returns: ('asset_invoice', 'asset, cost', [(1, 100), (40, 150.29)], '?, ?')
+        """
+        input = ('asset_invoice', [{'asset':1, 'cost':100}, {'asset':40, 'cost':150.29}])
+        (table, cols, vals, prep) = query_utils._parse_db_args_dict(input)
+        assert table == 'asset_invoice'
+        assert cols == 'asset, cost'
+        assert vals == [(1, 100), (40, 150.29)]
+        assert prep == '?, ?'
+    
+    def test__parse_db_args_dict_3(self, db_conn):
+        """
+        Parses ('asset_invoice', [])
+        Throws error because list is empty.
+        """
+        input = ('asset_invoice', [])
+        try:
+            query_utils._parse_db_args_dict(input)
+        except:
+            assert True
+            return
+        assert False
+
+    def test__parse_db_args_dict_4(self, db_conn):
+        """
+        Parses ('asset_invoice', [{'asset':1, 'desc':'desc here'}, {'asset':40, 'desc':'desc here'}])
+        Returns: ('asset_invoice', 'asset, desc', [(1, 'desc here'), (40, 'desc here'')], '?, ?')
+        """
+        input = ('asset_invoice', [{'asset':1, 'desc':'desc here'}, {'asset':40, 'desc':'desc here'}])
+        (table, cols, vals, prep) = query_utils._parse_db_args_dict(input)
+        assert table == 'asset_invoice'
+        assert cols == 'asset, desc'
+        assert vals == [(1, 'desc here'), (40, 'desc here')]
+        assert prep == '?, ?'
+    
+    def test__parse_db_args_dict_5(self, db_conn):
+        """
+        Parses ('asset_invoice', [{'asset':1, 'cost':100}, {'cost':150.29, 'asset':40}])
+        Returns: ('asset_invoice', 'asset, cost', [(1, 100), (40, 150.29)], '?, ?'),
+        even though keys are not in the same order between input dicts.
+        """
+        input = ('asset_invoice', [{'asset':1, 'cost':100}, {'cost':150.29, 'asset':40}])
+        (table, cols, vals, prep) = query_utils._parse_db_args_dict(input)
+        assert table == 'asset_invoice'
+        assert cols == 'asset, cost'
+        assert vals == [(1, 100), (40, 150.29)]
+        assert prep == '?, ?'
+    
+    def test__parse_db_args_dict_6(self, db_conn):
+        """
+        Parses ('asset_invoice', [{'asset':1, 'cost':100}, {'assets':40, 'cost':150.29}])
+        Throws error because dicts do not contain the same set of keys.
+        """
+        input = ('asset_invoice', [{'asset':1, 'cost':100}, {'assets':40, 'cost':150.29}])
+        try:
+            query_utils._parse_db_args_dict(input)
+        except:
+            assert True
+            return
+        assert False
+
+    def test__parse_db_args_dict_7(self, db_conn):
+        """
+        Parses ('asset_invoice', [{'asset':1, 'cost':100}, {'assets':40}])
+        Throws error because dicts do not contain the same number of pairs.
+        """
+        input = ('asset_invoice', [{'asset':1, 'cost':100}, {'assets':40}])
+        try:
+            query_utils._parse_db_args_dict(input)
+        except:
+            assert True
+            return
+        assert False
