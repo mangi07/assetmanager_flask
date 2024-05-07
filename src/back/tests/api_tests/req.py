51e8fe8e94e318982103c9c8ab398b67d0f27c8d
mangi07 <mangi07@users.noreply.github.com>
2024-05-07 11:16:33 +1000

    Finishing API tests for image and file requests.  Adding test for request of locations hierarchy.

--- src/back/tests/api_tests/req.py
+++ src/back/tests/api_tests/req.py
@@ -397,21 +397,36 @@ curl -X GET "http://localhost:5000/img/assets/1.jpg?file_access_token=$FILE_ACCE
 Example response: <the image requested>
 '''
 import shutil
-def get_image(file_name, dest):
-    #headers = {'Authorization': f'Bearer {user.access_token}'}
-    url = f'http://localhost:5000/img/assets/{file_name}?file_access_token={user.file_access_token}'
+
+def _get_file(url, dest):
     response = requests.get(url, stream=True)
     with open(dest, 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
 
+def get_image(file_name, dest):
+    url = f'http://localhost:5000/img/assets/{file_name}?file_access_token={user.file_access_token}'
+    _get_file(url, dest)
+    #response = requests.get(url, stream=True)
+    #with open(dest, 'wb') as out_file:
+    #    shutil.copyfileobj(response.raw, out_file)
+    #del response
+
+def get_file(file_name, dest):
+    url = f'http://localhost:5000/file/{file_name}?file_access_token={user.file_access_token}'
+    _get_file(url, dest)
+    #response = requests.get(url, stream=True)
+    #with open(dest, 'wb') as out_file:
+    #    shutil.copyfileobj(response.raw, out_file)
+    #del response
+
 # #############################################################################
-# Assert that the downloaded image is correct, by comparing the hashe of the
-# picture file on the server to the hash of the picture file downloaded.
+# Assert that the downloaded file is correct, by comparing the hash of the
+# file on the server to the hash of the file downloaded.
 #
 # If the hashes of these two files are the same, this gives a strong indication
 # that they are indeed the same files.  In other words, the request for the 
-# picture file succeeded.
+# file succeeded.
 import hashlib
 
 def get_file_hash(f):
@@ -425,18 +440,49 @@ def get_file_hash(f):
     req_hash =  hasher.hexdigest()
     return req_hash
 
-def test_correct_file(file_requested, file_expected):
-    get_image(file_requested, 'downloaded.JPG')
+def test_correct_file(test_method, file_requested, file_expected):
+    #get_image(file_requested, 'downloaded.JPG')
+    test_method(file_requested, 'downloaded.JPG')
     f1_hash = get_file_hash('downloaded.JPG')
     f2_hash = get_file_hash(file_expected)
     print(f1_hash)
     print(f2_hash)
     return f1_hash == f2_hash
 
-if test_correct_file('1.JPG', '../../db/files/assets/1.JPG'):
+# Test /img/<file_name> route
+if test_correct_file(get_image, '1.JPG', '../../db/files/assets/1.JPG'):
+    print("File requested as expected.")
+if test_correct_file(get_image, 'non_existant_file.JPG', '../../db/files/file_not_found.jpg'):
     print("File requested as expected.")
 
-if test_correct_file('non_existant_file.JPG', '../../db/files/file_not_found.jpg'):
+# Test /file/<file_name> route
+if test_correct_file(get_file, 'invoices/1a.pdf', '../../db/files/invoices/1a.pdf'):
     print("File requested as expected.")
+if test_correct_file(get_file, 'non_existant_file.JPG', '../../db/files/file_not_found.jpg'):
+    print("File requested as expected.")
+
+# #############################################################################
+# Get location listing
+# #############################################################################
+'''
+curl -X GET "http://localhost:5000/locations" -H "Authorization: Bearer $TOKEN"
+
+Example response: json representing hierarchy of locations..
+
+{'locations': {'1': {'description': 'root', 'parent': None},
+               '2': {'description': 'subA', 'parent': 1},
+               '3': {'description': 'subB', 'parent': 1},
+               '4': {'description': 'subA-1', 'parent': 2},
+               '5': {'description': 'subB-1', 'parent': 3},
+               '6': {'description': 'subB-2', 'parent': 3}}}
+'''
+def get_location_listing(user:User):
+    headers = {'Authorization': f'Bearer {user.access_token}'}
+    url = 'http://localhost:5000/locations'
+    location_listing = requests.get(f'{url}', headers=headers)
+    return location_listing
+
+locs = get_location_listing(user)
+#pprint(locs.json())
 
 
